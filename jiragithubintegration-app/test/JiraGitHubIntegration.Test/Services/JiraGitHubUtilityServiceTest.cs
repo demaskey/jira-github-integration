48a37e47f629433c1cdbfcf758004f611ddf83bc
Christoph DeMaskey <DEMASKEYC@science.regn.net>
2018-08-07 16:35:49 -0400

    add a unit test for update jira issue with github code review comment method

--- jiragithubintegration-app/test/JiraGitHubIntegration.Test/Services/JiraGitHubUtilityServiceTest.cs
+++ jiragithubintegration-app/test/JiraGitHubIntegration.Test/Services/JiraGitHubUtilityServiceTest.cs
@@ -1,4 +1,5 @@
 using JiraGitHubIntegration.GitHubModels;
+using JiraGitHubIntegration.JiraModels;
 using JiraGitHubIntegration.Services;
 using Moq;
 using Xunit;
@@ -9,10 +10,25 @@ namespace JiraGitHubIntegration.Tests.Services {
         public void GetIssueKeyFromBranchName_Test () {
             // Arrange
             var mockEvent = new Event ();
-            var jiraIssueKeyConventionSvc = new JiraGitHubUtilityService ();
+            var jiraGitHubUtilitySvc = new JiraGitHubUtilityService ();
 
             // Act
-            var issueKey = jiraIssueKeyConventionSvc.GetIssueKeyFromBranchName (mockEvent);
+            var issueKey = jiraGitHubUtilitySvc.GetIssueKeyFromBranchName (mockEvent);
+
+            // Assert
+        }
+
+        [Fact]
+        public void UpdateJiraIssueWithGitHubCodeReviewComment_Test () {
+            // Arrange
+
+            var jiraGitHubUtilitySvc = new JiraGitHubUtilityService ();
+
+            var mockGitHubEvent = new Event ();
+            var mockJiraIssue = new Issue ();
+
+            // Act
+            var updatedIssue = jiraGitHubUtilitySvc.UpdateJiraIssueWithGitHubCodeReviewComment (mockGitHubEvent, mockJiraIssue);
 
             // Assert
         }
